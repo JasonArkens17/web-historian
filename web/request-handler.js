e0d3e2ea2776b4008cdb0164220c5995f3372685
Jason Davenport <jason.arkens17@gmail.com>
2016-09-15 14:27:24 -0700

    refreshing request-handler

--- web/request-handler.js
+++ web/request-handler.js
@@ -1,7 +1,54 @@
 var path = require('path');
 var archive = require('../helpers/archive-helpers');
-// require more modules/folders here!
+
+var url = require('url');
+var helpers = require('./http-helpers');
+
+var actions = {
+  'GET': function(request, response) {
+    var urlPath = url.parse(request.url).pathname;
+
+    if (urlPath === '/') { urlPath = '/index.html'; }
+
+    helpers.serveAssets(response, urlPath, function() {
+      if (urlPath[0] === '/') { urlPath = urlPath.slice(1); }
+
+      archive.isUrlInList(urlPath, function(found) {
+        if (found) {
+          helpers.sendRedirect(response, '/loading.html');
+        } else {
+          helpers.send404(response);
+        }
+      });
+    });
+  },
+  'POST': function(request, response) {
+    helpers.collectData(request, function(data) {
+      var url = data.split('=')[1].replace('http://', '');
+      archive.isUrlInList(url, function(found) {
+        if (found) { // found site
+          archive.isUrlArchived(url, function(exists) {
+            if (exists) {
+              helpers.sendRedirect(response, '/' + url);
+            } else {
+              helpers.sendRedirect(response, '/loading.html');
+            }
+          });
+        } else {
+          archive.addUrlToList(url, function() {
+            helpers.sendRedirect(response, '/loading.html');
+          });
+        }
+      });
+    });
+  }
+};
 
 exports.handleRequest = function (req, res) {
-  res.end(archive.paths.list);
+  var handler = actions[req.method];
+  if (handler) {
+    handler(req, res);
+  } else {
+    helpers.send404(response);
+  }
 };
